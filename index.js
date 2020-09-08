e10a6e04b6d1736263736dba2dcdd9bdb68b0aa1
Cezar <cezar.sousa@sysmap.com.br>
2020-09-08 14:34:20 -0300

    Refactoring server listener

--- index.js
+++ index.js
@@ -43,6 +43,6 @@ const server = new ApolloServer({
   resolvers
 });
 
-server.listen().then(({ url }) => {
+server.listen(process.env.PORT || 5000).then(({ url }) => {
   console.log(`🚀 Server ready at ${url}`);
 });
